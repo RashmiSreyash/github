6ea67fedb19e829a62c969870200b4036eff55b9
RASHMI <RASHMI@LAPTOP-AHA78SJE>
2022-01-20 22:49:31 +0530

    2nd push

---
+++ com.crm.sdet28.githubPractice/src/test/java/com/crm/sdet28/githubPractice/com/crm/sdet28/githubPractice/Test3.java
@@ -0,0 +1,13 @@
+package com.crm.sdet28.githubPractice.com.crm.sdet28.githubPractice;
+
+import org.testng.annotations.Test;
+
+public class Test3 
+{
+	@Test
+	public void testMethod3()
+	{
+		System.out.println("this is 3rd test class");
+	}
+
+}
